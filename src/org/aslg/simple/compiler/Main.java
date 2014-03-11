8b43125909fd8cbda9b717bc04794248250c8e77
Andun S.L. Gunawardana <andunslg@gmail.com>
2014-03-11 22:11:08 +0530

    Adding packages

---
+++ src/org/aslg/simple/compiler/Main.java
@@ -0,0 +1,11 @@
+package org.aslg.simple.compiler;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: AndunSLG
+ * Date: 3/11/14
+ * Time: 10:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
